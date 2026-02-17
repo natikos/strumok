637b810aa063c43344746cf7973077233d41e687
natikos <natalia.kostelna20@gmail.com>
2026-02-17 13:18:36 +0200

    feat: update TypeScript configuration and Vite aliases for improved module resolution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@features": fileURLToPath(new URL("./src/features", import.meta.url)),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      "@shared": fileURLToPath(new URL("./src/shared", import.meta.url)),
     },
   },
 });
